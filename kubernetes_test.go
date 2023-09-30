c5de7e67c7694ac87c5af06ff96e6344a3d043cd
Justinas <12399634+justinas-b@users.noreply.github.com>
2023-09-30 04:31:37 +0300

    ci: adding unit tests (#20)
    ci: refactoring kubernetes related methods to accept kubernetes ClientSet as an input argument so that it could be mocked, removing ENV VAR support from getCurrentNamespace() method as it now can be passed as a CLI argumenet
    ci: adding unit tests

---
+++ kubernetes_test.go
@@ -0,0 +1,332 @@
+package main
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestGetConfigMap(t *testing.T) {
+	// Test when ConfigMap does not exist
+	t.Run("ConfigMap does not exist", func(t *testing.T) {
+
+		fakeClientSet := fake.NewSimpleClientset()
+
+		_, err := getConfigMap(fakeClientSet, "NOT_EXISTING_CONFIGMAP", "NOT_EXISTING_NAMESPASCE")
+		if !errors.IsNotFound(err) {
+			t.Errorf("Got unexpected error: %s, was expecting to get NotFound", err)
+		}
+	})
+
+	// Test when ConfigMap exist
+	t.Run("ConfigMap exist", func(t *testing.T) {
+
+		want := &v1.ConfigMap{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ConfigMap",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "NOT_EXISTING_CONFIGMAP",
+				Namespace:   "NOT_EXISTING_NAMESPASCE",
+				Annotations: map[string]string{},
+			},
+		}
+
+		fakeClientSet := fake.NewSimpleClientset(want)
+
+		got, err := getConfigMap(fakeClientSet, want.Name, want.Namespace)
+
+		if err != nil {
+			t.Errorf("Got unexpected error: %s, was expecting to get nil", err)
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("getConfigMap() returned unexpected object: %+v, want %+v", got, want)
+		}
+	})
+}
+
+func TestSetConfigMap(t *testing.T) {
+
+	// Test when ConfigMap does no exist
+	t.Run("ConfigMap does not exist", func(t *testing.T) {
+
+		// Define a fake namespace
+		ns := &v1.Namespace{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Namespace",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "TEST_NAMESPACE",
+				Annotations: map[string]string{},
+			},
+		}
+
+		// Create a fake client
+		fakeClientSet := fake.NewSimpleClientset(ns)
+
+		// Define data for configMap
+		cmdata := map[string]string{
+			"mapAccounts": "[]\n",
+			"mapUsers":    "[]\n",
+			"mapRoles":    "- rolearn: arn:aws:iam::190058439852:role/platform-development-dev-eks2019102907023762960000000b\n  username: system:node:{{EC2PrivateDNSName}}\n  groups:\n    - system:bootstrappers\n    - system:nodes\n- rolearn: arn:aws:iam::190058439852:role/terraform-role\n  username: terraform:{{SessionName}}\n  groups:\n    - system:masters\n- rolearn: arn:aws:iam::190058439852:role/AWSReservedSSO_admin-dev-pu_07572db8b73986b8\n  username: admin:{{SessionName}}\n  groups:\n    - system:masters\n- username: admin:{{SessionName}}\n",
+		}
+
+		// Update configMap which does not exist (should create new configMap)
+		err := setConfigMap(fakeClientSet, "NOT_EXISTING_CONFIGMAP", ns.Name, cmdata)
+		if err != nil {
+			t.Errorf("Got unexpected error: %s, was expecting to get nil", err)
+		}
+
+		// Check if configMap was created
+		cm, err := fakeClientSet.CoreV1().ConfigMaps(ns.Name).Get(context.TODO(), "NOT_EXISTING_CONFIGMAP", metav1.GetOptions{})
+		if err != nil {
+			t.Errorf("Got unexpected error: %s, was expecting to get nil", err)
+		}
+
+		// Check if configMap created has data we expect
+		if !reflect.DeepEqual(cm.Data, cmdata) {
+			t.Errorf("setConfigMap() created unexpected object: %+v, want %+v", cm.Data, cmdata)
+		}
+
+	})
+
+	// Test when namespace does exist
+	t.Run("ConfigMap and Namespace does not exist", func(t *testing.T) {
+
+		var fakeClientSet *fake.Clientset = fake.NewSimpleClientset()
+
+		cmdata := map[string]string{
+			"mapAccounts": "[]\n",
+			"mapUsers":    "[]\n",
+			"mapRoles":    "- rolearn: arn:aws:iam::190058439852:role/platform-development-dev-eks2019102907023762960000000b\n  username: system:node:{{EC2PrivateDNSName}}\n  groups:\n    - system:bootstrappers\n    - system:nodes\n- rolearn: arn:aws:iam::190058439852:role/terraform-role\n  username: terraform:{{SessionName}}\n  groups:\n    - system:masters\n- rolearn: arn:aws:iam::190058439852:role/AWSReservedSSO_admin-dev-pu_07572db8b73986b8\n  username: admin:{{SessionName}}\n  groups:\n    - system:masters\n- username: admin:{{SessionName}}\n",
+		}
+
+		err := setConfigMap(fakeClientSet, "NOT_EXISTING_CONFIGMAP", "NOT_EXISTING_NAMESPACE", cmdata)
+		if err != nil {
+			t.Errorf("Got unexpected error: %s, was expecting to get nil", err)
+		}
+
+		// Check if configMap was created
+		cm, err := fakeClientSet.CoreV1().ConfigMaps("NOT_EXISTING_NAMESPACE").Get(context.TODO(), "NOT_EXISTING_CONFIGMAP", metav1.GetOptions{})
+		if err != nil {
+			t.Errorf("Got unexpected error: %s, was expecting to get nil", err)
+		}
+
+		// Check if configMap created has data we expect
+		if !reflect.DeepEqual(cm.Data, cmdata) {
+			t.Errorf("setConfigMap() created unexpected object: %+v, want %+v", cm.Data, cmdata)
+		}
+	})
+
+	// Test when ConfigMap does not exist
+	t.Run("ConfigMap does exist", func(t *testing.T) {
+
+		// Define a fake namespace
+		ns := &v1.Namespace{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "Namespace",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "TEST_NAMESPACE",
+				Annotations: map[string]string{},
+			},
+		}
+
+		// Define a fake configMap
+		cm := &v1.ConfigMap{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "ConfigMap",
+				APIVersion: "v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:        "TEST_CONFIGMAP",
+				Namespace:   ns.Name,
+				Annotations: map[string]string{},
+			},
+			Data: map[string]string{
+				"mapAccounts": "[]\n",
+				"mapUsers":    "[]\n",
+				"mapRoles":    "- rolearn: arn:aws:iam::190058439852:role/platform-development-dev-eks2019102907023762960000000b\n  username: system:node:{{EC2PrivateDNSName}}\n  groups:\n    - system:bootstrappers\n    - system:nodes\n\n",
+			},
+		}
+
+		// Create a fake client
+		fakeClientSet := fake.NewSimpleClientset(ns, cm)
+
+		cmdata := map[string]string{
+			"mapAccounts": "[]\n",
+			"mapUsers":    "[]\n",
+			"mapRoles":    "- rolearn: arn:aws:iam::190058439852:role/platform-development-dev-eks2019102907023762960000000b\n  username: system:node:{{EC2PrivateDNSName}}\n  groups:\n    - system:bootstrappers\n    - system:nodes\n- rolearn: arn:aws:iam::190058439852:role/terraform-role\n  username: terraform:{{SessionName}}\n  groups:\n    - system:masters\n- rolearn: arn:aws:iam::190058439852:role/AWSReservedSSO_admin-dev-pu_07572db8b73986b8\n  username: admin:{{SessionName}}\n  groups:\n    - system:masters\n- username: admin:{{SessionName}}\n",
+		}
+
+		err := setConfigMap(fakeClientSet, cm.Name, ns.Name, cmdata)
+		if err != nil {
+			t.Errorf("Got unexpected error: %s, was expecting to get nil", err)
+		}
+
+		// Check if configMap was created
+		updatedConfigMap, err := fakeClientSet.CoreV1().ConfigMaps(ns.Name).Get(context.TODO(), cm.Name, metav1.GetOptions{})
+		if err != nil {
+			t.Errorf("Got unexpected error: %s, was expecting to get nil", err)
+		}
+
+		// Check if configMap created has data we expect
+		if !reflect.DeepEqual(updatedConfigMap.Data, cmdata) {
+			t.Errorf("setConfigMap() created unexpected object: %+v, want %+v", cm.Data, cmdata)
+		}
+
+	})
+}
+
+func TestTransformRoleMappings(t *testing.T) {
+	// Test when IAM role does not exist for provided PermissionSet
+	t.Run("IAM role does not exist for provided permission set name", func(t *testing.T) {
+		mappings := []SSORoleMapping{
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_devops_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+			{
+				RoleARN:       "",
+				PermissionSet: "sre",
+				Username:      "",
+				Groups:        []string{},
+			},
+		}
+
+		roles := []types.Role{
+			{
+				RoleName: aws.String("AWSReservedSSO_devops_0123456789abcdef"),
+				Path:     aws.String("/aws-reserved/sso.amazonaws.com/eu-west-1/"),
+				Arn:      aws.String("arn:aws:iam::123456789012:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_devops_0123456789abcdef"),
+			},
+		}
+
+		want := []SSORoleMapping{
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_devops_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+		}
+
+		got := transformRoleMappings(mappings, roles)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("TransformRoleMappings() returned unexpected object: %+v, want %+v", got, want)
+		}
+	})
+
+	// Test when []SSORoleMapping does not need translation (no permissionSet names are provided)
+	t.Run("Provided input does not need transformation", func(t *testing.T) {
+		mappings := []SSORoleMapping{
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_devops_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_sre_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+		}
+
+		roles := []types.Role{
+			{
+				RoleName: aws.String("AWSReservedSSO_devops_0123456789abcdef"),
+				Path:     aws.String("/aws-reserved/sso.amazonaws.com/eu-west-1/"),
+				Arn:      aws.String("arn:aws:iam::123456789012:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_devops_0123456789abcdef"),
+			},
+		}
+
+		want := []SSORoleMapping{
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_devops_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_sre_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+		}
+
+		got := transformRoleMappings(mappings, roles)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("TransformRoleMappings() returned unexpected object: %+v, want %+v", got, want)
+		}
+	})
+
+	// Test when PermissionSet name was provided and corresponding AWS IAM role exists
+	t.Run("Translate permissionSet name to role ARN", func(t *testing.T) {
+		mappings := []SSORoleMapping{
+			{
+				RoleARN:       "",
+				PermissionSet: "devops",
+				Username:      "",
+				Groups:        []string{},
+			},
+			{
+				RoleARN:       "",
+				PermissionSet: "sre",
+				Username:      "",
+				Groups:        []string{},
+			},
+		}
+
+		roles := []types.Role{
+			{
+				RoleName: aws.String("AWSReservedSSO_devops_0123456789abcdef"),
+				Path:     aws.String("/aws-reserved/sso.amazonaws.com/eu-west-1/"),
+				Arn:      aws.String("arn:aws:iam::123456789012:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_devops_0123456789abcdef"),
+			},
+			{
+				RoleName: aws.String("AWSReservedSSO_sre_0123456789abcdef"),
+				Path:     aws.String("/aws-reserved/sso.amazonaws.com/eu-west-1/"),
+				Arn:      aws.String("arn:aws:iam::123456789012:role/aws-reserved/sso.amazonaws.com/eu-west-1/AWSReservedSSO_sre_0123456789abcdef"),
+			},
+		}
+
+		want := []SSORoleMapping{
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_devops_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+			{
+				RoleARN:       "arn:aws:iam::123456789012:role/AWSReservedSSO_sre_0123456789abcdef",
+				PermissionSet: "",
+				Username:      "",
+				Groups:        []string{},
+			},
+		}
+
+		got := transformRoleMappings(mappings, roles)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("TransformRoleMappings() returned unexpected object: %+v, want %+v", got, want)
+		}
+	})
+}
